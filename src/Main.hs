7d7f7caae58629b9dbbaa5a204aa991d68483a06
Richard Cook <rcook@rcook.org>
2018-08-18 21:07:35 -0700

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Sound.Pulse.Simple
+
+main :: IO ()
+main=do
+  s<-simpleNew Nothing "example" Play Nothing "this is an example application"
+      (SampleSpec (F32 LittleEndian) 44100 1) Nothing Nothing
+  simpleWrite s ([sin $ 2*pi*440*(t/44100)|t<-[1..44100*10]] :: [Float])
+  simpleDrain s
+  simpleFree s
